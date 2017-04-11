18b80c96108e26b2c4a9962094a6830813dcde38
Yang minqiang <obsicn@hotmail.com>
2017-04-11 12:17:50 +0800

    configure for mysql

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_log',
+        'USER': 'admin',
+        'PASSWORD': 'Xjtu1nic',
+        'HOST': '10.0.3.149',
+        'PORT': '3306',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
